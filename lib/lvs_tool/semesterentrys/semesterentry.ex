e544f85d7a5884379e259b453b0ab0d345feb5b4
LBBumann <dontodenz@gmail.com>
2025-07-11 18:07:00 +0200

    for next move: adding lvs field for sum of LVS from all courses vice versa - connections to relavant databaseentries

--- lib/lvs_tool/semesterentrys/semesterentry.ex
+++ lib/lvs_tool/semesterentrys/semesterentry.ex
@@ -5,8 +5,15 @@ defmodule LvsTool.Semesterentrys.Semesterentry do
   schema "semesterentrys" do
     field :name, :string
     field :status, :string
+    # field :lvs, :float
 
     belongs_to :user, LvsTool.Accounts.User
+
+    # has_many :standard_course_entries, LvsTool.Courses.StandardCourseEntry
+    # has_many :project_entries, LvsTool.Semesterentrys.ProjectEntry
+    # has_many :excursion_entries, LvsTool.Semesterentrys.ExcursionEntry
+    # has_many :thesis_entries, LvsTool.Semesterentrys.ThesisEntry
+    # has_many :reduction_entries, LvsTool.Semesterentrys.ReductionEntry
 
     timestamps(type: :utc_datetime)
   end
