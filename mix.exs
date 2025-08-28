d086111ebf03849644658c0cc5bc4b0e97247294
LBBumann <dontodenz@gmail.com>
2025-08-28 12:59:33 +0200

    added dotenv to read .env-File

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule LvsTool.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:dotenv, "~> 3.0", only: [:dev, :test]},
       {:oauth2, "~> 2.0"},
       {:argon2_elixir, "~> 3.0"},
       {:phoenix, "~> 1.7.21"},
