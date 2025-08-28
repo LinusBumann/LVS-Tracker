1ba9edd6ce1bab41c50180a396e159a0eadddaf9
LBBumann <dontodenz@gmail.com>
2025-08-28 14:26:36 +0200

    updated redirect if User selected to keep him loged in

--- lib/lvs_tool_web/controllers/page_controller.ex
+++ lib/lvs_tool_web/controllers/page_controller.ex
@@ -2,8 +2,13 @@ defmodule LvsToolWeb.PageController do
   use LvsToolWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    # Wenn ein Benutzer eingeloggt ist, leite direkt zu /semesterentrys weiter
+    if conn.assigns[:current_user] do
+      redirect(conn, to: ~p"/semesterentrys")
+    else
+      # The home page is often custom made,
+      # so skip the default app layout.
+      render(conn, :home, layout: false)
+    end
   end
 end
--- test/lvs_tool_web/controllers/page_controller_test.exs
+++ test/lvs_tool_web/controllers/page_controller_test.exs
@@ -1,8 +1,17 @@
 defmodule LvsToolWeb.PageControllerTest do
   use LvsToolWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
+  import LvsTool.AccountsFixtures
+
+  test "GET / redirects to /semesterentrys when user is logged in", %{conn: conn} do
+    user = user_fixture()
+    conn = log_in_user(conn, user)
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert redirected_to(conn) == ~p"/semesterentrys"
+  end
+
+  test "GET / renders home page when user is not logged in", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Plane dein Semester"
   end
 end
